ed4253c89261fc3bb6597c35dd99f9aa38396133
Maickel Pirchiner <maickelpc@gmail.com>
2020-10-24 10:58:40 -0300

    Criação de seeds ,correção de relação user-schedul

---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // create permissions
+        Permission::create(['name' => 'edit schedules']);
+        Permission::create(['name' => 'delete schedules']);
+
+        // create roles and assign created permissions
+        $role = Role::create(['name' => 'admin'])
+            ->givePermissionTo(['edit schedules', 'delete schedules']);
+
+    
+    }
+}
---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+        User::create([ 
+            'name' => 'Jorge', 
+            'email' => 'jorge@gmail.com', 
+            'password' => bcrypt('123456')
+        ]);
+
+        $clara = User::create([ 
+            'name' => 'Clara', 
+            'email' => 'clara@gmail.com', 
+            'password' => bcrypt('123456')
+        ]);
+        
+        $roleAdmin = Role::where('name', 'admin')->first();
+        $clara->assignRole($roleAdmin);
+
+    }
+}
