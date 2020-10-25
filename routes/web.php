21e21cb796f8ba7c0c02860f93881994c065418c
Maickel Pirchiner <maickelpc@gmail.com>
2020-10-25 18:28:20 -0300

    Metodos de Autenticacao + remocao CSRF

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,19 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'App\Http\Controllers\AuthController@login');
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+    Route::post('me', 'App\Http\Controllers\AuthController@me');
+
+});
 
 Route::get('/', function () {
     return view('welcome');
